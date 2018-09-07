a7b007735f69efb0c814a867b4140825966fc4f2
Kirk Scheibelhut <kjs@google.com>
2018-09-07 10:08:49 -0700

    Add ability to find matches using Fzf

--- fuzzy.go
+++ fuzzy.go
@@ -3,7 +3,12 @@
 package fuzzy
 
 import (
+	"fmt"
+	"io"
 	"math"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/antzucaro/matchr"
 )
@@ -35,4 +40,48 @@ func Match(s string, cs []string) (string, float64) {
 
 func norm(s float64, a string, b string) float64 {
 	return s / math.Max(float64(len(a)), float64(len(b)))
+}
+
+// Returns the match from the corpus cs selected by fzf.
+func FzfMatch(cs []string) (string, error) {
+	if !available("fzf") {
+		return "", fmt.Errorf("fzf is not available")
+	}
+	return withFilter("fzf", func(in io.WriteCloser) {
+		for _, c := range cs {
+			fmt.Fprintln(in, c)
+		}
+	})
+}
+
+func available(name string) bool {
+	cmd := exec.Command(shell(), "-c", "command -v "+name)
+	err := cmd.Run()
+	return err == nil
+}
+
+func withFilter(command string, input func(in io.WriteCloser)) (string, error) {
+	cmd := exec.Command(shell(), "-c", command)
+	cmd.Stderr = os.Stderr
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+	go func() {
+		input(in)
+		in.Close()
+	}()
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+func shell() string {
+	sh := os.Getenv("SHELL")
+	if len(sh) == 0 {
+		sh = "sh"
+	}
+	return sh
 }
